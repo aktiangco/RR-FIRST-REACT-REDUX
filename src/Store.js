38cc3b25962999820e07f31211945872dc80909a
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-03-06 22:54:27 -0800

    plugging counterSlice into Store

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
+import counterSlice from './features/counterSlice'
 
 export const store = configureStore({
-    reducer: {}
+    reducer: counterSlice
 })
