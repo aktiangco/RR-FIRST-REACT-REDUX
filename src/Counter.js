fc82395716d2ada2583cc8513c601e15fe85ca05
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-03-06 23:05:47 -0800

    Building UI Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,34 +1,26 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment, incrementByAmount } from './features/counterSlice'
+import { increment, decrement } from './features/counterSlice'
 
-const Counter = () => {
-    const count = useSelector((state) => state.counter.value)
+function Counter() {
+    // Call useSelector to grab the current value of our state variable
+    // from the store, and assign it to a variable named "count"
+    const count = useSelector((state) => state.value)
+    // And include the useDispatch hook...
     const dispatch = useDispatch()
-    const [input, setInput] = useState(0)
 
-    const byAmount = (e) => {
-        e.preventDefault()
-        dispatch(incrementByAmount(Number(input)))
-    }
 
     return (
         <div>
-            <h1>{count}</h1>
-            <button
-                aria-label="Increment value"
-                onClick={() => dispatch(increment())}>
+            <h1>
+                {count}
+            </h1>
+            <button onClick={() => dispatch(increment())}>
                 Increment
             </button>
-            <button
-                aria-label="Decrement value"
-                onClick={() => dispatch(decrement())}>
+            <button onClick={() => dispatch(decrement())}>
                 Decrement
             </button>
-            <form onSubmit={(e) => byAmount(e)}>
-                <input type="number" onChange={(e) => setInput(e.target.value)} />
-                <button type="submit">Submit</button>
-            </form>
         </div>
     )
 }
